a5401104154d0241851bd07064e14ae7f5ec0883
jamshid <jamshidshamshod0705@gmail.com>
2023-09-07 11:46:30 +0500

    update email.js

--- utility/email.js
+++ utility/email.js
@@ -1,26 +1,53 @@
 const nodemailer=require('nodemailer')
+const pug=require('pug')
+const htmltotext=require('html-to-text')
 
-const sendEmail=async (options)=>{
-  // 1 transporter yaratish
-  const transporter=nodemailer.createTransport({
-    host:process.env.EMAIL_HOST,
-    port:process.env.EMAIL_PORT,
-    auth:{
-      user:process.env.EMAIL_USERNAME,
-      pass:process.env.EMAIL_PASSWORD
-    }
-  })
-  
-  // 2 mail ni sozlash
-  const mailOption={
-    from:'jamshid khatamov <jamshidshamshod0705@gamil.com>',
-    to:options.email,
-    subject:options.subject,
-    text:options.message
+module.exports=class Email{
+  constructor(user,url){
+    this.to=user.email,
+    this.firstName=user.firstName.split(' ')[0],
+    this.url=url,
+    this.from=`jamshid khatamov <${process.env.EMAIL_FROM}>`
   }
-  // 3 mail ni jo'natish
-  await transporter.sendMail(mailOption)
 
+  newTransport(){
+    if(process.env.NODE_ENV='production'){
+      return 1
+    }
+
+    return nodemailer.createTransport({
+      host:process.env.EMAIL_HOST,
+      port:process.env.EMAIL_PORT,
+      auth:{
+        user:process.env.EMAIL_USERNAME,
+        pass:process.env.EMAIL_PASSWORD
+      }
+    })
+  }
+
+  async send(template,subject){
+    //1 pug render qilish
+    const html=pug.renderFile(`${__dirname}/../views/emails/${template}.pug`,{
+      firstName:this.firstName,
+      url:this.url,
+      subject:subject
+    })
+    //2 mailoption ni
+    const mailOption={
+    from:this.from,
+    to:this.to,
+    subject:subject,
+    html:html,
+    text:htmltotext.fromString(html)
+  }
+    //3 send qilish emailga
+    await this.newTransport.sendMail(mailOption)
+  }
+
+  async sendWelcome(){
+    await this.send('welcome','Welcome to Natours Family !')
+  }
 }
+
 
 module.exports=sendEmail
